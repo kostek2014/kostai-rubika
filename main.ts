ee67b3e2e655ed0c2b8b9aa90b2678d019f5690d
kostek2014 <136499893+kostek2014@users.noreply.github.com>
2023-06-13 20:13:54 +0200

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(0, 10))
+    basic.showNumber(randint(0, 10))
+    basic.showNumber(randint(0, 10))
+    basic.showNumber(randint(0, 10))
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playMelody("D B C F C5 A E G ", 120)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(randint(0, 10))
+    basic.showNumber(randint(0, 10))
+    basic.showNumber(randint(0, 10))
+    basic.showNumber(randint(0, 10))
+})
